a26c7d9547468c8407982cc4658f6246a3f80dcd
fatourati <71594532+fatourati@users.noreply.github.com>
2021-04-25 14:30:00 +0000

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -1,7 +1,3 @@
-! wget 
-#! /bin/bash
-printf "setup loading" >&2
-{
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
@@ -20,12 +16,4 @@ sudo apt -y install firefox
 sudo apt -y install qbittorrent
 sudo apt install nload
 sudo adduser ALOK chrome-remote-desktop
-} &> /dev/null &&
-printf "\nSetup Completed " >&2 ||
-printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
-read -p "Paste Here: " CRP
-su - ALOK -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n'
-printf 'Your SUDO Pasword Is 8426 \n'
- &> /dev/null
+
