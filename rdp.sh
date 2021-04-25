f26f63084c5eb28f18b5982946f626d948dd2330
fatourati <71594532+fatourati@users.noreply.github.com>
2021-04-25 21:13:18 +0000

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -7,6 +7,7 @@ sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -22,4 +23,6 @@ printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
+su - ALOK -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
+printf 'Your SUDO Pasword Is 8426 \n'
