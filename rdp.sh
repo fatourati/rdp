6f92ee5a51a9976fe716ce0df8bc62f4af3a4e35
fatourati <71594532+fatourati@users.noreply.github.com>
2021-06-24 17:36:50 +0100

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -23,6 +23,5 @@ printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - ALOK -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
 printf 'Your SUDO Pasword Is 8426 \n'
